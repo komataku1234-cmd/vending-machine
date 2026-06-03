92645af6e577e34c973689d1add870c651b3a27e
user <user@usernoMacBook-Air.local>
2026-06-03 15:40:00 +0900

    second commit

--- buy.js
+++ buy.js
@@ -1,20 +1,28 @@
-//npm install prompt-sync ターミナルでこれを入力してインストールする.  
-
 const merchandise = [
   ["水", 100],
   ["お茶", 120],
   ["コーヒー", 150]
 ];
-const prompt = require("prompt-sync")();
-const money = prompt("投入金額を入力してください: ");
-console.log(`${money}円を入れました`);
-for(let i = 0; i < merchandise.length; i++) {
-  console.log(i+":"+merchandise[i][0] + "：" + merchandise[i][1] + "円");
-}
-const buy = prompt("何を買いますか?番号でお願いします");
-console.log(buy);
-if(merchandise[buy][1]<money){
-    console.log(`${merchandise[buy][0]}を購入しました`);
-}else{
-    console.log(`お金が足りないので${merchandise[buy][0]}買えませんでした`);
-}
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("投入金額を入力してください: ", (money) => {
+    console.log(`${money}円を入れました`);
+    for(let i = 0; i < merchandise.length; i++) {
+    console.log(i+":"+merchandise[i][0] + "：" + merchandise[i][1] + "円");
+    }
+    rl.question("何を買いますか?番号でお願いします", (buy) => {
+        if(merchandise[buy][1]<money){
+            console.log(`${merchandise[buy][0]}を購入しました`);
+            console.log(`残り${money-merchandise[buy][1]}円です`);
+        }else{
+            console.log(`お金が足りないので${merchandise[buy][0]}買えませんでした`);
+        }
+        rl.close();
+    });
+});
